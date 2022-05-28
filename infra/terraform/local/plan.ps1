b42b35503b79b4d7ce748788b0ad47277c8dee1d
Sietse Trommelen <3702986+SietseT@users.noreply.github.com>
2022-05-28 17:54:56 +0200

    Add first Terraform scripts

---
+++ infra/terraform/local/plan.ps1
@@ -0,0 +1,29 @@
+$localEnvFile = "local.env"
+
+if(!(Test-Path -Path $localEnvFile)) {
+  Throw "local.env not found."
+}
+
+switch -File $localEnvFile {
+  default {
+    $name, $value = $_.Trim() -split '=', 2
+    if ($name -and $name[0] -ne '#') { # ignore blank and comment lines.
+      Set-Item "Env:$name" $value
+    }
+  }
+}
+
+Push-Location "../"
+
+try {
+
+terraform init `
+  -backend-config="storage_account_name=$Env:TF_VAR_azure_state_storage_account" `
+  -backend-config="key=$($Env:TF_VAR_state_filename).tfstate" `
+  -backend-config="access_key=$Env:TF_VAR_azure_stage_storage_account_accesskey"
+
+terraform plan -out=local/tfplan
+}
+catch { }
+
+Pop-Location
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "azure_subscription_id" {
+  type = string
+}
+
+variable "azure_tenant_id" {
+  type = string
+}
+
+variable "azure_state_storage_account" {
+  type = string
+}
+
+variable "state_filename" {
+  type = string
+}

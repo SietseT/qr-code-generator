5e688400287125e52f3e830970f443e42aa1ada1
Sietse Trommelen <3702986+SietseT@users.noreply.github.com>
2022-05-29 11:44:17 +0200

    Renamed resource group

--- infra/terraform/base.tf
+++ infra/terraform/base.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "resource-group" {
-  name     = "reg-dev-qrgenerator"
+  name     = "rg-qrgenerator-${var.environment_name}"
   location = "westeurope"
 }
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -7,9 +7,21 @@ variable "azure_tenant_id" {
 }
 
 variable "azure_state_storage_account" {
-  type = string
+  type        = string
+  description = "Name of storage account in Azure to use as backend for state storage."
 }
 
 variable "state_filename" {
-  type = string
+  type        = string
+  description = "Name of the state file."
+}
+
+variable "environment_name" {
+  type        = string
+  description = "Environment name"
+
+  validation {
+    condition     = var.environment_name == "dev" || var.environment_name == "tst" || var.environment_name == "acc" || var.environment_name == "prd"
+    error_message = "Environment name must be either: dev, tst, acc or prd."
+  }
 }
